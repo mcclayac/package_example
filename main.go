eb642163c1d6b5ddbd01591fe9f124ff106d47d2
anthonymcclay <anthony.mcclay@gmail.com>
2026-06-27 09:56:04 -0500

    ran functions in package

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	do_format "github.com/mcclayac/package_example/do-format"
+	"github.com/mcclayac/package_example/math"
+)
 
 func main() {
 	fmt.Println("Run main in package: ")
 	fmt.Println("module github.com/mcclayac/package_example")
+
+	num := math.Double(2)
+	output := do_format.Number(num)
+	fmt.Println(output)
+
 }
