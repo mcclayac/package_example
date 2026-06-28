39349214b1ba2dfa687b6a7ae8514ba665da6602
anthonymcclay <anthony.mcclay@gmail.com>
2026-06-28 10:38:27 -0500

    More Package Examples

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 	"github.com/mcclayac/package_example/math"
 )
 
+//http://localhost:8080
+//go install golang.org/x/pkgsite/cmd/pkgsite@latest
+//pkgsite
+
 func main() {
 	fmt.Println("Run main in package: ")
 	fmt.Println("module github.com/mcclayac/package_example")
