4b8fdce9ef8a8afba0251cf05a1782d68c85caa9
Anthony McClay <anthony.mcclay@gmail.com>
2026-06-29 09:32:35 -0500

    update math

--- math/math.go
+++ math/math.go
@@ -1,5 +1,12 @@
+// The package was created to separate
+// the double function
+// package math
+//
+// created by Anthony McClay
 package math
 
+// Double - doubles the given int value
+// created by : Anthony McClay
 func Double(x int) int {
 	return x * 2
 }
